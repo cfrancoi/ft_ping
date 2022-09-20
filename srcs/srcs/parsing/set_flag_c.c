d61e2e069a154ffbb5986dbea6589ec95c71eb22
cfrancoi <corentinfrancois31@gmail.com>
2022-09-20 15:49:44 +0200

    [parsing] get -c value

--- srcs/srcs/parsing/set_flag_c.c
+++ srcs/srcs/parsing/set_flag_c.c
@@ -1,9 +1,13 @@
 #include "options.h"
 #include "debug.h"
+#include "libft.h"
 
 int set_flag_c(const char *c, t_options *opts, int flag)
 {
 	(void)set_simple_flag(c, opts, flag);
+
+	opts->count = ft_atoi(c);
+	
 	debug_print("FLAG_C: %s\n", c);
 	return (1);
 }
